740a92776d8d65522d26eef8589ca3bc2bc42b6b
mrmorgan-i <misraelkelly@gmail.com>
2025-08-04 19:49:24 -0500

    Add some react useState functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
   return (
     <div className="px-4 py-20">
       <h1>QR Code Generator</h1>
       <p>Generate QR codes for your links</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <p>Count: {count}</p>
       <Link href="/about">About</Link>
     </div>
   );
-} 
+}
