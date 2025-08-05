6a073b3cb7761feddac89424e70b3bf5798d934b
dmich-dev <michaud671468@newmanu.edu>
2025-08-04 19:11:21 -0500

    Hello kelly daddy this will cause a merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="px-4 py-20">
       <h1>QR Code Generator</h1>
+      <i> This Kelly and mines QR code generator</i>
       <Link href="/about">About</Link>
     </div>
   );
