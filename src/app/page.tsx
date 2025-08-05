bfd341de33c820a8fecb8d3d78a13b4ab4173022
mrmorgan-i <misraelkelly@gmail.com>
2025-08-04 19:03:38 -0500

    trigger merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="px-4 py-20">
       <h1>QR Code Generator</h1>
+      <p>Generate QR codes for your links</p>
       <Link href="/about">About</Link>
     </div>
   );
